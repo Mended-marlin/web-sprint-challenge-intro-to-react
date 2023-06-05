13f0ba2b5963b34778233be028f6cea8ffb10d79
mended-marlin <felix71679@gmail.com>
2023-06-04 21:36:24 -0400

    Initial set up and data calls

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
+import axios from "axios"
+import Character from "./components/Character"
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [data, setData] = useState([])
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+
+  useEffect(()=>{
+    axios.get("https://swapi.dev/api/people/")
+      .then(res => {
+        setData(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        !data ? <div>... Loading ...</div> :
+        data.map((char,i) => {
+        return (<Character character={char} key={i} />)
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
-// Write your Character component here
+import React from "react"
+
+
+export default function Character(props) {
+    const {character} = props
+    console.log(character)
+    if(!character) {
+        return (<div>... loading ...</div>)
+
+    }
+    return (
+        <div className="character">
+            <h3>{character.name}</h3>
+            
+        </div>
+    )
+}
